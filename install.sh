68d6794807e6dc64588717b16f1caa579fcb75b0
evolonation <1838940019@qq.com>
2023-03-04 20:37:03 +0800

    add dashboard

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ gunzip clash-linux-${1}-v1.13.0.gz
 mv -T clash-linux-${1}-v1.13.0.gz /usr/local/bin/clash
 cp config.yaml /etc/clash/
 cp Country.mmdb /etc/clash/
+cp -r clash-dashborad /etc/clash/clash-dashboard
 cp clash.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start clash.service
