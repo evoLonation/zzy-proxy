36671ca489672b9403b7598ec777b64a0000d40b
evolonation <1838940019@qq.com>
2023-03-04 21:41:25 +0800

    complete

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 echo "your architecture: ${1}"
 echo "your bash init file: ${2}"
-cat ${2} > initfile
-gunzip clash-linux-${1}-v1.13.0.gz
-mv -T clash-linux-${1}-v1.13.0.gz /usr/local/bin/clash
+echo ${2} > initfile
+gunzip -k clash-linux-${1}-v1.13.0.gz
+mkdir -p /usr/local/bin
+mv -T clash-linux-${1}-v1.13.0 /usr/local/bin/clash
+chmod +x /usr/local/bin/clash
+mkdir -p /etc/clash
 cp config.yaml /etc/clash/
 cp Country.mmdb /etc/clash/
-cp -r clash-dashborad /etc/clash/clash-dashboard
+mkdir -p /etc/clash/clash-dashboard
+cp -r clash-dashboard /etc/clash/
 cp clash.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start clash.service
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
-echo "your bash init file: ${1}"
+echo "your bash init file: `cat initfile`"
 rm /usr/local/bin/clash
 rm -r /etc/clash/
 rm  /etc/systemd/system/clash.service
-systemctl daemon-reload
-sed -i -f addin `cat initfile`
+# systemctl daemon-reload
+sed -i '/alias openproxy/d' `cat initfile`
+sed -i '/alias closeproxy/d' `cat initfile`
