07cd9f9a0b8c46cb1d7b3fe400d0635df6b8f4ef
evolonation <1838940019@qq.com>
2023-04-01 23:31:15 +0800

    add just

--- install.sh
+++ install.sh
@@ -13,10 +13,13 @@ initfile=`. ./getinitfile.sh ${user}`
 echo "your architecture: ${arch}"
 echo "your user: ${user}"
 echo "your initfile: ${initfile}"
-exit
-gunzip -k clash-linux-${1}-v1.13.0.gz
+
+echo "uninstall first"
+. ./uninstall.sh ${user}
+
+gunzip -k clash-linux-${arch}-v1.13.0.gz
 mkdir -p /usr/local/bin
-mv -T clash-linux-${1}-v1.13.0 /usr/local/bin/clash
+mv -T clash-linux-${arch}-v1.13.0 /usr/local/bin/clash
 chmod +x /usr/local/bin/clash
 mkdir -p /etc/clash
 cp config.yaml /etc/clash/
@@ -27,6 +30,6 @@ cp clash.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start clash.service
 systemctl enable clash.service
-cat addin >> ${2}
+cat addin >> ${initfile}
 sed -i '/http_proxy/d' /etc/sudoers
 echo "Defaults env_keep += \"http_proxy https_proxy no_proxy\"" >> /etc/sudoers
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,21 @@
-echo "your bash init file: `cat initfile`"
+#!/bin/bash
+user=${1}
+if [ $USER != "root" ]; then
+	echo "this script must executed by root, now is ${USER}"
+	exit
+fi
+if [ -z $1 ]; then 
+	echo "usage: ${0} user"
+	exit
+fi
+initfile=`. ./getinitfile.sh ${user}`
+echo "your bash init file: ${initfile}"
 systemctl stop clash
 rm  /etc/systemd/system/clash.service
 systemctl daemon-reload
 rm /usr/local/bin/clash
 rm -r /etc/clash/
-sed -i '/alias openproxy/d' `cat initfile`
-sed -i '/alias closeproxy/d' `cat initfile`
-sed -i '/openproxy/d' `cat initfile`
+sed -i '/alias openproxy/d'  ${initfile}
+sed -i '/alias closeproxy/d'  ${initfile}
+sed -i '/openproxy/d'  ${initfile}
 sed -i '/http_proxy/d' /etc/sudoers
